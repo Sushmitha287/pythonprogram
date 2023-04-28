c078b32feeb9fd0fc2f40805c1d212fbbb883d5e
Sushmitha287 <130962732+Sushmitha287@users.noreply.github.com>
2023-04-28 16:30:36 +0530

    sushmitha_listmethods

---
+++ list.py
@@ -0,0 +1,19 @@
+# list()
+#split()
+#To get information about list:
+n=[1,2,2,2,3,3,4]
+#len()
+#n.count(ele)
+#n.index(ele)
+#in,not in
+#Manipulating Elements of List:
+#n.append(ele)-to add item at the end of the list.
+#n.insert(pos,ele)-to insert item at specified index position.
+#l1.extend(l2)-To add all items of one list to another list
+#n.remove(ele)-to remove specified item from the list.
+#n.pop()-It removes and returns the last element of the list.
+#n.pop(index)-To remove and return element present at specified index.
+#n.reverse()-to reverse order of elements of list.
+#sort()-to sort the elements of list according to default natural sorting order
+#copy()
+#‘=‘ operator meant for aliasing, ‘copy() function’ meant for cloning.
